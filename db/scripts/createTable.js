02f719cf629a70c8973c0d447301def7763aecd1
Cloe <cloe@cloesavannah.com>
2022-06-29 11:54:03 +0100

    writing docs comments

--- db/scripts/createTable.js
+++ db/scripts/createTable.js
@@ -1,3 +1,12 @@
+/*
+This file:
+Creates the tables on the Heroku database 
+Tables already created - only needs to be called once (do not call again!)
+Script for calling this file is in package.json
+
+Uses query function to interact with database credentials as environment variables using dotenv package.
+ */
+
 import { query } from "../index.js";
 
 const topicsSqlString = "CREATE TABLE IF NOT EXISTS topics (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, topic VARCHAR(255), subtopic VARCHAR(255), weektopic VARCHAR(255), summary TEXT, syntax TEXT, complete BOOL, week INT, day INT);";
